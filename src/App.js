dab3520630de38f59d4bb6803a0a1e016b2d2116
Jin Choo <jinchoo315@gmail.com>
2021-09-01 15:16:30 -0500

    add page route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import AddDevice from "./page/AddDevice";
+import Dashboard from "./page/Dashboard";
+import UpdateDevice from "./page/UpdateDevice";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mx-auto">
+      <Router>
+        <header class="header">
+          <Link to="/">
+            <img
+              class="header__logo"
+              src="/asset/img/logo.png"
+              alt="Ninja RMM"
+            />
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/add-device">
+            <AddDevice />
+          </Route>
+          <Route path="/:deviceId">
+            <UpdateDevice />
+          </Route>
+          <Route path="/">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

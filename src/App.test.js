d9233e5c5f5652b1fcf811496f79f4903b67f8b2
Jin Choo <jinchoo315@gmail.com>
2021-09-06 18:40:31 -0500

    simple test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import AddDevice from "./page/AddDevice";
+import UpdateDevice from "./page/UpdateDevice";
 
-test('renders learn react link', () => {
+
+test('renders dashboard screen when app run', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const pageTitle = screen.getByText('Dashboard');
+  expect(pageTitle).toBeInTheDocument();
 });
+
+test('render add device screen', () => {
+  render(<AddDevice />);
+  const pageTitle = screen.getByText('Add Device');
+  expect(pageTitle).toBeInTheDocument();
+});
+
+// test('render update device screen', () => {
+//   render(<UpdateDevice />);
+//   const pageTitle = screen.getByText(/Updating: .*/);
+//   expect(pageTitle).toBeInTheDocument();
+// });
